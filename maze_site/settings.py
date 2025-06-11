056b208d491072a454f72479f20cc2868f559b6e
YuehJung Lee <emma30533@gmail.com>
2025-06-12 00:09:23 +0800

    fix: add Heroku host to ALLOWED_HOSTS

--- maze_site/settings.py
+++ maze_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['maze-solver-demo-6b4cadc93820.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
