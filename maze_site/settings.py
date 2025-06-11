2367fd5a4a210eaf7467778c6dd4754cee8a0c01
YuehJung Lee <emma30533@gmail.com>
2025-06-12 00:52:55 +0800

    Switch database to Heroku Postgres

--- maze_site/settings.py
+++ maze_site/settings.py
@@ -73,13 +73,12 @@ WSGI_APPLICATION = "maze_site.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+import dj_database_url
+import os
 
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
